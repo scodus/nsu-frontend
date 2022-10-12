938628784cd6616010c71c5667634083d3665761
brraut <bishowraut@gmail.com>
2022-10-12 10:32:13 +0545

    page added

---
+++ src/pages/robowars/index.jsx
@@ -0,0 +1,17 @@
+const Hackathon = () => {
+  return (
+    <>
+      <div className="home-bg-copy">
+        <div className="home-wrapper">
+          <div className="home-main-title linear-wipe">Hackathon</div>
+        </div>
+      </div>
+      <div className="home-bg">
+        <div className="home-wrapper">
+          <div className="home-main-title linear-wipe">Hackathon</div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Hackathon;
