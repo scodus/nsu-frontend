4111d353ab19336c0737e1a7f2e7e8bef66751d6
bishalkharell <bishalkharel4321@gmail.com>
2022-10-18 10:08:49 +0545

    animation transition changed

--- src/components/Animations/AnimatedPage.jsx
+++ src/components/Animations/AnimatedPage.jsx
@@ -9,7 +9,7 @@ const animations = {
 
 function AnimatedPage({ children }) {
     return (
-        <motion.div   variants={animations} initial="initial" animate="animate"  transition = {{duration : 0.75}}  >
+        <motion.div   variants={animations} initial="initial" animate="animate" exit="exit"  transition = {{duration : 0.5}}  >
             {children}
         </motion.div>
     )
